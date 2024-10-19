f86b11272c2ab77e221b2c8e09d269ccb9e305df
gauravgangwarweb <gauravgangwar501@gmail.com>
2024-10-19 12:14:12 +0530

    Initialized the Nav component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="bg-lightNav flex justify-between px-4 py-2">
+            <img loading="lazy" src="" alt="" />
+        </nav>
+    );
+};
+
+export default Navbar;
